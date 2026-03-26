0748d13e732c29714c09647663800e7a11fd932a
Danny Rehelis <autogun@gmail.com>
2026-03-26 21:05:34 +0000

    cr: copilot

--- src/views/storage/__tests__/BucketBrowserView.test.ts
+++ src/views/storage/__tests__/BucketBrowserView.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
-import { mount, flushPromises } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import BucketBrowserView from '../BucketBrowserView.vue'
 import { useStorageStore } from '@/stores/storage'
